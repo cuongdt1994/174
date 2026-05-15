7c640d1825b49eb5777c9a859105c7d681cdc8e0
John 🪿 <63875204+cuongdt1994@users.noreply.github.com>
2026-05-15 19:55:42 +0700

    Update player.cpp

--- cgame/gs/player.cpp
+++ cgame/gs/player.cpp
@@ -10844,14 +10844,14 @@ void gplayer_imp::SendAllData(bool detail_inv, bool detail_equip, bool detail_ta
 	TreasureInfo();
 	GetLibItems();
 	//Kid
-	_player_kid.ClientSync(player_kid::kid_val1)
-	_player_kid.ClientSync(player_kid::kid_val4)
-	_player_kid.ClientSync(player_kid::kid_val5)
-	_player_kid.ClientSync(player_kid::kid_val15)
+	_player_kid.ClientSync(1);
+	_player_kid.ClientSync(4);
+	_player_kid.ClientSync(5);
+	_player_kid.ClientSync(15);
 	
 	//home_task_info
-	_runner->home_task_info(64,_home_task_info.task_trigger_times,_home_task_info.task_refresh_times)
-	
+	_runner->home_task_info(64,_home_task_info.task_trigger_times,_home_task_info.task_refresh_times);
+
 	/*170+*/
 	if(_passwd_safe.passwd_active != 2)
 	{
